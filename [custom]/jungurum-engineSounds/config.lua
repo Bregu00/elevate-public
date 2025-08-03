70a40c80f4c3877a0e89eb0d7333e6d4fdae69dc
Bregu00 <Mathiaslarsen2005@gmail.com>
2025-08-03 22:46:21 +0200

    Update enginesounds config.lua

--- [custom]/jungurum-engineSounds/config.lua
+++ [custom]/jungurum-engineSounds/config.lua
@@ -1,5 +1,3 @@
-Config = Config or {}
-
 Config.debug = false
 
 Config.Companies = {
@@ -34,68 +32,67 @@ Config.Companies = {
 
 Config.engineCategories = {
     {
-        categoriName = "Supercar",
+        categoryName = "Supercar",
         engineTypes = {
-            {name = "zentorno",                 label = "Zentorno Engine"},
-            {name = "t20",                      label = "T20 Engine"},
-            {name = 'ferrarif140fe',            label = 'Ferrari F140FE Engine'},
-            {name = 'c6v8sound',                label = 'Corvette C6 Engine'},
-            {name = 'diablov12',                label = 'Lamborghini Diablo GT Engine'},
-            {name = 'sestov10',                 label = 'Lamborghini Sesto Engine'},
-            {name = 'urusv8',                   label = 'Lamborghini Urus Engine'},
-            {name = 'perfov10',                 label = 'Lamborghini Huracan Engine'},
-            {name = 'viperv10',                 label = 'Dodge Viper Engine'},
-            {name = 'gtaspanov10',              label = 'GTA Spano Engine'},
-            {name = 'mclarenv8',                label = 'McLaren 720s Engine'},
-            {name = 'm840trsenna',              label = 'McLaren Senna Engine'},
+            {name = "zentorno", label = "Zentorno Engine"},
+            {name = "t20", label = "T20 Engine"},
+            {name = 'ferrarif140fe', label = 'Ferrari F140FE Engine'},
+            {name = 'c6v8sound', label = 'Corvette C6 Engine'},
+            {name = 'diablov12', label = 'Lamborghini Diablo GT Engine'},
+            {name = 'sestov10', label = 'Lamborghini Sesto Engine'},
+            {name = 'urusv8', label = 'Lamborghini Urus Engine'},
+            {name = 'perfov10', label = 'Lamborghini Huracan Engine'},
+            {name = 'viperv10', label = 'Dodge Viper Engine'},
+            {name = 'gtaspanov10', label = 'GTA Spano Engine'},
+            {name = 'mclarenv8', label = 'McLaren 720s Engine'},
+            {name = 'm840trsenna', label = 'McLaren Senna Engine'},
         }
     },
     {
-        categoriName = "JDM",
+        categoryName = "JDM",
         engineTypes = {
-            {name = 'elegy2',                   label = 'ElegyRH8 Engine'},
-            {name = 'legacycl01kcmsubwrx',      label = 'Impreza Engine'},
-            {name = 'ta013vq35',                label = '350Z Engine'},
-            {name = 'aq2jzgterace',             label = 'Supra 2JZ Engine'},
-            {name = 'aq31maz13btune',           label = 'Mazda RX-7 Engine'},
-            {name = 'rb26dett',                 label = 'R32 Engine'},
-            {name = 'bnr34ffeng',               label = 'R34 Engine'},
-            {name = 'r35sound',                 label = 'R35 Engine'},
-            {name = 'aq57mit4g63t',             label = 'Lancer EVO Engine'},
-            {name = 'ta4b11',                   label = 'Lancer EVO 10 Engine'},
+            {name = 'elegy2', label = 'ElegyRH8 Engine'},
+            {name = 'legacycl01kcmsubwrx', label = 'Impreza Engine'},
+            {name = 'ta013vq35', label = '350Z Engine'},
+            {name = 'aq2jzgterace', label = 'Supra 2JZ Engine'},
+            {name = 'aq31maz13btune', label = 'Mazda RX-7 Engine'},
+            {name = 'rb26dett', label = 'R32 Engine'},
+            {name = 'bnr34ffeng', label = 'R34 Engine'},
+            {name = 'r35sound', label = 'R35 Engine'},
+            {name = 'aq57mit4g63t', label = 'Lancer EVO Engine'},
+            {name = 'ta4b11', label = 'Lancer EVO 10 Engine'},
         }
     },
     {
-        categoriName = "Sport",
+        categoryName = "Sport",
         engineTypes = {
-            {name = 'banshee',                  label = 'Banshee Engine'},
-            {name = 'penumbra',                 label = 'Penumbra Engine'},
-            {name = 'taaud40v8',                label = 'Audi Engine #1'},
-            {name = 'audiwx',                   label = 'Audi Engine #2'},
-            {name = 'audi7a',                   label = 'Audi Engine #3'},
-            {name = 'audiea855',                label = 'Audi Engine #4'},
-            {name = 'n55b30t0',                 label = 'BMW M2 Engine'},
-            {name = 's55b30',                   label = 'BMW M3 Engine'},
-            {name = 'tagt3flat6',               label = 'Porsche 911 Engine'},
+            {name = 'banshee', label = 'Banshee Engine'},
+            {name = 'penumbra', label = 'Penumbra Engine'},
+            {name = 'taaud40v8', label = 'Audi Engine #1'},
+            {name = 'audiwx', label = 'Audi Engine #2'},
+            {name = 'audi7a', label = 'Audi Engine #3'},
+            {name = 'audiea855', label = 'Audi Engine #4'},
+            {name = 'n55b30t0', label = 'BMW M2 Engine'},
+            {name = 's55b30', label = 'BMW M3 Engine'},
+            {name = 'tagt3flat6', label = 'Porsche 911 Engine'},
         }
     },
     {
-        categoriName = "Muscle",
+        categoryName = "Muscle",
         engineTypes = {
-            {name = "buffalo",                  label = "Buffalo Engine"},
-            {name = 'aqls7raceswap',            label = 'V8 Engine'},
-            {name = 'tamustanggt50',            label = 'Mustang Engine #1'},
-            {name = 'tascmustanggt50',          label = 'Mustang Engine #2'},
-
+            {name = "buffalo", label = "Buffalo Engine"},
+            {name = 'aqls7raceswap', label = 'V8 Engine'},
+            {name = 'tamustanggt50', label = 'Mustang Engine #1'},
+            {name = 'tascmustanggt50', label = 'Mustang Engine #2'},
         }
     },
     {
-        categoriName = "Motorcycles",
+        categoryName = "Motorcycles",
         engineTypes = {
-            {name = 'suzukigsxr1k',             label = 'Suzuki GSX-R 1000 Engine'},
-            {name = 'kc12r1200gsakrapovic',     label = 'BMW R1200GS Engine'},
-            {name = 'kc144kawazx10rsc',         label = 'Kawasaki Ninja ZX-10R Engine'},
-            {name = 'ta103ninjah2r',            label = 'Kawasaki Ninja H2R Engine'},
+            {name = 'suzukigsxr1k', label = 'Suzuki GSX-R 1000 Engine'},
+            {name = 'kc12r1200gsakrapovic', label = 'BMW R1200GS Engine'},
+            {name = 'kc144kawazx10rsc', label = 'Kawasaki Ninja ZX-10R Engine'},
+            {name = 'ta103ninjah2r', label = 'Kawasaki Ninja H2R Engine'},
         }
     }
 }
